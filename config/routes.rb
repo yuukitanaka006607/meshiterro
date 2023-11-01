75f5429524af014805bdf71b548d662e584aeb6f
YuukiTanaka <145189789+yuukitanaka006607@users.noreply.github.com>
2023-11-01 11:17:53 +0000

    [Finish]4-8

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'homes/about' => 'homes#about',as:'about'
 end

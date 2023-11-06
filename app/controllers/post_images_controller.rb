8d541b87fbbd44b19d78f965d5b99edb69b6aa4f
YuukiTanaka <145189789+yuukitanaka006607@users.noreply.github.com>
2023-11-06 00:56:27 +0000

    [Finish]4-21

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -6,9 +6,12 @@ class PostImagesController < ApplicationController
  def create
    @post_image = PostImage.new(post_image_params)
    @post_image.user_id = current_user.id
-   @post_image.save
+   if @post_image.save
    redirect_to post_images_path
+ else
+   render :new
  end
+end
 
  def destroy
    post_image =PostImage.find(params[:id])
